91a5922525abea0733736e79d81e8f71ad0bed2c
codim <n.ioelants@hotmail.com>
2021-11-12 07:29:17 +0100

    Ajout d'une tâche

--- src/js/modules/TodoList.js
+++ src/js/modules/TodoList.js
@@ -15,13 +15,45 @@ export default class TodoList {
       this.todos.push(new Todo({parent: this, todo}));
     }
   }
+  /**
+   * Rendu du TodoList
+   * @return {[return]} [description]
+   */
   render () {
     this.el.innerHTML = this.template;
-    // L'élément .todo-list existe pour le navigateur
+    // Le DOM de la liste existe pour le navigateur
     this.listEl = this.el.querySelector('.todo-list');
-  
+    // Rendu des todos
     for (let todo of this.todos) {
       todo.render();
     }
+    // Activation des éléments interactifs
+    this.activateBtns();
+  }
+
+/**
+ * Ajout d'un todo
+ */
+  addTodo () {
+    const content = this.el.querySelector('.new-todo').value;
+    const id = this.todos[this.todos.length - 1].id + 1;
+    const todo = {id, content, completed:false};
+    const newTodo = new Todo({parent: this, todo});
+    this.todos.push(newTodo);
+    newTodo.render();
+    this.el.querySelector('.new-todo').value = "";
+  }
+
+  /**
+   * Activation des éléments interactifs de la TodoList
+   * @return {[type]} [description]
+   */
+  activateBtns () {
+    // Activation de l'input .new-todo
+    this.el.querySelector('.new-todo').onkeyup = (e) => {
+      if (e.keyCode === 13) {
+        this.addTodo();
+      }
+    };
   }
 }
